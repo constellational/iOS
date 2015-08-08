0597fca5c386c7e29ea82ebdf8f22d359a5e97f8
Arpith Siromoney <arpith@feedreader.co>
2015-08-08 17:40:22 +0530

    add style to settings

--- components/SettingsPage.js
+++ components/SettingsPage.js
@@ -11,16 +11,22 @@ var {
 
 class SettingsPage extends React.Component {
   render() {
-    var title = 'Settings';
-    var instructions = 'Pick a username';
-    if (this.props.username) instructions = 'Change your username';
-    else title = 'Welcome!';
+    if (this.props.username) {
+      var title = 'Settings';
+      var instructions = 'Change your username';
+      var returnKeyType = 'done';
+    } else {
+      var title = 'Welcome!';
+      var instructions = 'Pick a username';
+      var returnKeyType = 'join';
+    }
     return (
       <View style={styles.page}>
         <Text style={styles.heading}>{title}</Text>
         <Text style={styles.subheading}>{instructions}</Text>
         <TextInput 
-          returnKeyType='done'
+          keyboardType='url'
+          returnKeyType={returnKeyType}
           value={this.props.username}
           autofocus={true}
           style={styles.input}
@@ -40,6 +46,25 @@ var styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  heading: {
+    fontSize: 42,
+    padding: 10,
+  },
+  subheading: {
+    fontSize: 24,
+    padding: 8,
+  },
+  input: {
+    alignSelf: 'center',
+    margin: 10,
+    marginBottom: 120,
+    height: 46,
+    width: 150,
+    textAlign: 'center',
+    borderWidth: 0.5,
+    borderColor: 'black',
+    borderRadius: 5,
   },
 });
 
