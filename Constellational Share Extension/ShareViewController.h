dcf9a54618197a4f1b85898e2606757233315fec
Arpith Siromoney <arpith@feedreader.co>
2015-11-26 23:52:34 +0530

    getting started with app extensions (share via constellational)

---
+++ Constellational Share Extension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  Constellational Share Extension
+//
+//  Created by Arpith Siromoney on 26/11/15.
+//  Copyright © 2015 Facebook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
