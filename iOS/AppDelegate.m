1b989a05728ed135c89c02dcec2b7896bd46d82c
Arpith Siromoney <arpith@feedreader.co>
2015-12-05 17:38:52 +0530

    updates

--- iOS/AppDelegate.m
+++ iOS/AppDelegate.m
@@ -31,7 +31,7 @@
    * on the same Wi-Fi network.
    */
 
- // jsCodeLocation = [NSURL URLWithString:@"http://10.0.0.11:8081/index.ios.bundle"];
+//  jsCodeLocation = [NSURL URLWithString:@"http://10.0.0.11:8081/index.ios.bundle"];
 
   /**
    * OPTION 2
